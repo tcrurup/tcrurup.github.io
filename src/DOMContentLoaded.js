ff67e5e00a255c76b88b4035dfb9304e36ef0f9f
tcrurup <49696152+tcrurup@users.noreply.github.com>
2021-04-20 09:08:13 -0600

    Push to test window.location

--- src/DOMContentLoaded.js
+++ src/DOMContentLoaded.js
@@ -1,5 +1,6 @@
 const baseUrl = "http://tcrurup.github.io/index"
 
 document.addEventListener("DOMContentLoaded", event => {
-    console.log("DOM is loaded");
+    console.log("DOM Loaded");
+    console.log(window.location);
 })
