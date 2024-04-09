83fcdc3d24507a404acfaef67cc72164cd73edf2
tcrurup <49696152+tcrurup@users.noreply.github.com>
2024-04-09 14:15:49 -0600

    test push to git hub for new navbar

---
+++ js/components/navbar.js
@@ -0,0 +1,31 @@
+const navBar = [
+    {
+        innerHTML: "Home", 
+        href: "/"
+    },
+    {
+        innerHTML: "Blog",
+        href: "/blog/"
+    },
+    {
+        innerHTML: "Zero Player Game", 
+        href: "/zeroPlayerGame/"
+    }
+]
+
+const createNavBar = () =>{
+    let navBarElement = document.createElement("nav")
+    
+    navBar.forEach(nav => {
+        let link = document.createElement("a")
+        link.href = nav.href;
+        link.innerHTML = nav.innerHTML;
+        navBarElement.append(link)
+    })
+
+    return navBarElement
+}
+
+
+
+document.body.prepend(createNavBar())
